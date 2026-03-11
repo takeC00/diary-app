19e2b739c1e81b90ec5abdf673c58a33161b99f5
takeC00 <take.614@icloud.com>
2026-03-12 02:00:43 +0900

    /をログイン画面に設定、ログイン画面に現在日時を動的に表示

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -21,7 +21,7 @@
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
-
+            <h1 class="mt-5">{{ now()->format('Y-m-d H:i:s') }}</h1>
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
