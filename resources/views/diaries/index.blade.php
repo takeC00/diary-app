45761f17bdf20db6cfc275390eab8e562b79587b
takeC00 <take.614@icloud.com>
2026-03-14 00:53:19 +0900

    Controller、Route作成

---
+++ resources/views/diaries/index.blade.php
@@ -0,0 +1,13 @@
+<div class="w-40 bg-white border border-gray-200 rounded-lg shadow overflow-hidden">
+
+    <img
+        src="/images/pic-1.jpg"
+        class="block w-full !h-16 object-cover"
+        alt=""
+    >
+
+    <div class="p-2 text-center">
+        <p class="text-xs font-semibold">sample</p>
+    </div>
+
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DiaryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,5 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+
+Route::get('diaries', [DiaryController::class, "index"])->name('diaries.index');
 
 require __DIR__.'/auth.php';
